a40333543174f48c81482428bd37288d2c322351
Mike Rousos <Mike@MJRousos.com>
2019-09-04 18:08:04 -0400

    Add transparency migration sample

---
+++ Transparency/NetFx/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security;
+
+[assembly: AllowPartiallyTrustedCallers]
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calling critical method");
+            try
+            {
+                CriticalMethod();
+                Console.WriteLine("ERROR - Expected exception not thrown");
+            }
+            catch (MethodAccessException)
+            {
+                Console.WriteLine("Caught expected exception for going from Transparent -> Critical code");
+            }
+            SafeCriticalMethod();
+            Console.WriteLine("Done");
+        }
+
+        [SecuritySafeCritical]
+        private static void SafeCriticalMethod()
+        {
+            Console.WriteLine("In a safe critical method; calling a critical method");
+            CriticalMethod();
+        }
+
+        [SecurityCritical]
+        private static void CriticalMethod()
+        {
+            Console.WriteLine("In a critical method");
+        }
+    }
+}
