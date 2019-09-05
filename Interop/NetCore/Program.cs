61f0e912c1e69a8ecaf43726e6f27e931c867d43
Mike Rousos <Mike@MJRousos.com>
2019-09-05 15:23:03 -0400

    Add simple COM Interop example

---
+++ Interop/NetCore/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main()
+        {
+            var progId = "InternetExplorer.Application";
+            var type = Type.GetTypeFromProgID(progId);
+            dynamic inst = Activator.CreateInstance(type);
+
+            inst.Visible = true;
+            inst.Navigate("https://www.microsoft.com");
+        }
+    }
+}
